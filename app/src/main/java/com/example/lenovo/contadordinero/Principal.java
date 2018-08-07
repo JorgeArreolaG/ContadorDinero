da9dfb25634bd14f302dac1f825409ef91d5ed3f
Jorge Antonio Arreola Granillo <jorgearreola.g@outlook.com>
2018-08-07 14:54:34 -0600

    menu ok

--- app/src/main/java/com/example/lenovo/contadordinero/Principal.java
+++ app/src/main/java/com/example/lenovo/contadordinero/Principal.java
@@ -2,6 +2,7 @@ package com.example.lenovo.contadordinero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Principal extends AppCompatActivity {
 
@@ -10,4 +11,12 @@ public class Principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
     }
+
+
+    @Override public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
 }

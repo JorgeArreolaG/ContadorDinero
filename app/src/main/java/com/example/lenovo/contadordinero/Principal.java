883b66a96f194565a75efb93c94f5643744ffc04
Jorge Antonio Arreola Granillo <jorgearreola.g@outlook.com>
2018-08-07 17:12:03 -0600

    quienes somos 50%

--- app/src/main/java/com/example/lenovo/contadordinero/Principal.java
+++ app/src/main/java/com/example/lenovo/contadordinero/Principal.java
@@ -1,8 +1,11 @@
 package com.example.lenovo.contadordinero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class Principal extends AppCompatActivity {
 
@@ -12,11 +15,41 @@ public class Principal extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
     }
 
+    public void ejecutar_quienes_somos (View view){
+        Intent intent=new Intent (this, Quienes_somos.class);
+        startActivity(intent);
+    }
 
-    @Override public boolean onCreateOptionsMenu(Menu menu){
 
-        getMenuInflater().inflate(R.menu.menu,menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem opcion_menu) {
+
+        int id = opcion_menu.getItemId();
+
+        if (id == R.id.quienes_somos){
+            ejecutar_quienes_somos(null);
+        }
+
+        if (id == R.id.mas_app){
+            return  true;
+        }
+
+        if (id == R.id.compartir){
+            return  true;
+        }
+
+        if (id == R.id.valorar){
+            return  true;
+        }
+
+        return super.onOptionsItemSelected(opcion_menu);
+    }
 }
+

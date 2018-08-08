1f6bbe1b5c0483acb0ef720b659f64ad964c26da
Jorge Antonio Arreola Granillo <jorgearreola.g@outlook.com>
2018-08-07 19:22:26 -0600

    quienes somos 100%

--- app/src/main/java/com/example/lenovo/contadordinero/Quienes_somos.java
+++ app/src/main/java/com/example/lenovo/contadordinero/Quienes_somos.java
@@ -1,7 +1,11 @@
 package com.example.lenovo.contadordinero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Quienes_somos extends AppCompatActivity {
 
@@ -9,5 +13,47 @@ public class Quienes_somos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quienes_somos);
+        this.setTitle("¿Quiénes somos?");
+
+        Button contacto = (Button)findViewById(R.id.btnContacto);
+        Button ok = (Button)findViewById(R.id.btnok);
+
+        contacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarEmail();
+            }
+        });
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
+    }
+
+    private void enviarEmail(){
+        //Instanciamos un Intent del tipo ACTION_SEND
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        //Aqui definimos la tipologia de datos del contenido dle Email en este caso text/html
+        emailIntent.setType("text/html");
+        // Indicamos con un Array de tipo String las direcciones de correo a las cuales enviar
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"JorgeArreola.G@outlook.com"});
+        // Aqui definimos un titulo para el Email
+        emailIntent.putExtra(android.content.Intent.EXTRA_TITLE, "Contador de Dinero Mx");
+        // Aqui definimos un Asunto para el Email
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Contacto desde App Contador de Dinero Mx");
+        try {
+            //Enviamos el Correo iniciando una nueva Activity con el emailIntent.
+            startActivity(Intent.createChooser(emailIntent, "Enviar Correo a Servicio"));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(Quienes_somos.this, "No hay ningun cliente de correo instalado.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
+

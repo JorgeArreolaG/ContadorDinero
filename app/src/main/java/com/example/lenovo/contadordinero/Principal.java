91bcd0aa70544806651a3a26984c5999486659d6
Jorge Antonio Arreola Granillo <jorgearreola.g@outlook.com>
2018-08-13 19:13:03 -0600

    subTotal Ok

--- app/src/main/java/com/example/lenovo/contadordinero/Principal.java
+++ app/src/main/java/com/example/lenovo/contadordinero/Principal.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class Principal extends AppCompatActivity {
 
     @Override
@@ -22,12 +24,30 @@ public class Principal extends AppCompatActivity {
         final EditText et1000 = (EditText)findViewById(R.id.et1000);
         final EditText et500 = (EditText)findViewById(R.id.et500);
         final EditText et200 = (EditText)findViewById(R.id.et200);
-        EditText et100 = (EditText)findViewById(R.id.et100);
+        final EditText et100 = (EditText)findViewById(R.id.et100);
+        final EditText et50 = (EditText)findViewById(R.id.et50);
+        final EditText et20 = (EditText)findViewById(R.id.et20);
+        final EditText etm20 = (EditText)findViewById(R.id.etm20);
+        final EditText et10 = (EditText)findViewById(R.id.et10);
+        final EditText et5 = (EditText)findViewById(R.id.et5);
+        final EditText et2 = (EditText)findViewById(R.id.et2);
+        final EditText et1 = (EditText)findViewById(R.id.et1);
+        final EditText et05 = (EditText)findViewById(R.id.et05);
         final EditText etTotal = (EditText)findViewById(R.id.etTotal);
 
         final TextView lbl1000 = (TextView)findViewById(R.id.lbl1000);
         final TextView lbl500 = (TextView)findViewById(R.id.lbl500);
         final TextView lbl200 = (TextView)findViewById(R.id.lbl200);
+        final TextView lbl100 = (TextView)findViewById(R.id.lbl100);
+        final TextView lbl50 = (TextView)findViewById(R.id.lbl50);
+        final TextView lbl20 = (TextView)findViewById(R.id.lbl20);
+        final TextView lblm20 = (TextView)findViewById(R.id.lblm20);
+        final TextView lbl10 = (TextView)findViewById(R.id.lbl10);
+        final TextView lbl5 = (TextView)findViewById(R.id.lbl5);
+        final TextView lbl2 = (TextView)findViewById(R.id.lbl2);
+        final TextView lbl1 = (TextView)findViewById(R.id.lbl1);
+        final TextView lbl05 = (TextView)findViewById(R.id.lbl05);
+
 
 
 
@@ -35,6 +55,16 @@ public class Principal extends AppCompatActivity {
         lbl1000.setText("0");
         lbl500.setText("0");
         lbl200.setText("0");
+        lbl100.setText("0");
+        lbl50.setText("0");
+        lbl20.setText("0");
+        lblm20.setText("0");
+        lbl10.setText("0");
+        lbl5.setText("0");
+        lbl2.setText("0");
+        lbl1.setText("0");
+        lbl05.setText("0");
+
 
         et1000.addTextChangedListener(new TextWatcher() {
             @Override
